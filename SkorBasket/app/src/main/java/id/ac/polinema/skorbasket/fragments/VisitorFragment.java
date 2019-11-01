f03531e81f5b2bd64aa432fc808eff372440aff1
Anasziaulhaq <ziaulhaqanas59@gmail.com>
2019-11-01 14:16:55 +0700

    SkorBasket

--- SkorBasket/app/src/main/java/id/ac/polinema/skorbasket/fragments/VisitorFragment.java
+++ SkorBasket/app/src/main/java/id/ac/polinema/skorbasket/fragments/VisitorFragment.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -47,6 +49,8 @@ public class VisitorFragment extends Fragment {
 	@Override
 	public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
 		super.onViewCreated(view, savedInstanceState);
+		//instansiasi ViewModel
+		sharedScore = ViewModelProviders.of(requireActivity()).get(SharedScore.class);
 		scoreDuaVisitor = view.findViewById(R.id.scoreDuaVisitor);
 		scoreTigaVisitor = view.findViewById(R.id.scoreTigaVisitor);
 		scoreVisitor = view.findViewById(R.id.scoreVisitor);
@@ -54,5 +58,35 @@ public class VisitorFragment extends Fragment {
 
 
 		// Tambahkan logic tombol di bagian bawah ini
+		//informasi score visitor
+		sharedScore.getScoreVisitor().observe(requireActivity(), new Observer<Integer>() {
+			@Override
+			public void onChanged(Integer score) {
+				scoreVisitor.setText(String.valueOf(score));
+				scoreDefault = score;
+			}
+		});
+
+		//memperbarui informasi score
+		scoreDuaVisitor.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				sharedScore.setScoreVisitor(scoreDefault+2);
+			}
+		});
+
+		scoreTigaVisitor.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				sharedScore.setScoreVisitor(scoreDefault+3);
+			}
+		});
+
+		scoreSatuVisitor.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				sharedScore.setScoreVisitor(scoreDefault+1);
+			}
+		});
 	}
 }
